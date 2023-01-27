39286864040b06d9f9d23f57b9ec74fb8fbf1bb0
Fabio <fabio@tiva.app>
2023-01-27 00:34:46 -0300

    Improve interface with add and remove itens

--- app/controllers/cakes_controller.rb
+++ app/controllers/cakes_controller.rb
@@ -5,11 +5,17 @@ class CakesController < ApplicationController
 
     def calculate_cake 
         begin
-            @result = ApplicationController.helpers.cakes(eval(params[:recipe]) , eval(params[:ingredients]))
-
-            redirect_to root_path(cake_result: @result, recipe: params[:recipe], ingredients: params[:ingredients])
-        rescue
-            redirect_to root_path(notice: "error")
+            # Transform recipe and ingredients to correct format for the helper function
+            recipe = {}
+            ingredients = {}
+            params[:recipe].values.each { |r| recipe["#{r[:key]}"] = r[:value].to_i }
+            params[:ingredients].values.each { |i| ingredients["#{i[:key]}"] = i[:value].to_i }
+            
+            @result = ApplicationController.helpers.cakes(recipe , ingredients)
+            
+            redirect_to root_path(cake_result: @result, params: request.parameters)
+        rescue => error
+            redirect_back(fallback_location: root_path, notice: error)
         end 
     end
 end
